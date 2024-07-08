31af6e019b9561878a69c908a1d350e9544737c4
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-08 18:10:06 +0530

    refactored the code

--- check_limits.py
+++ check_limits.py
@@ -1,34 +1,32 @@
 def temperature_ok(temperature):
     if temperature >= 0 and temperature <= 45:
-        return True
+        return 1
     else:
         print('Temperature is out of range!')
-        return False
+        return 0
     
 
 def soc_ok(soc):
     if soc >=20 and soc <= 80:
-        return True
+        return 1
     else:
         print('State of Charge is out of range!')
-        return False
+        return 0
     
 
 def charge_rate_ok(charge_rate):
     if 0.8 > charge_rate:
-        return True       
+        return 1       
     else:
         print('charge rate is out of range!')
-        return False    
+        return 0    
       
     
 def battery_is_ok(temperature, soc, charge_rate):
            
-   temperature_1 = temperature_ok(temperature)
-   soc_2 = soc_ok(soc)
-   charge_rate_3 = charge_rate_ok(charge_rate)
+   battery = temperature_ok(temperature) + soc_ok(soc) + charge_rate_ok(charge_rate)
      
-   if temperature_1 and soc_2 and charge_rate_3:
+   if battery == 3:
        return True
    else:
        return False
